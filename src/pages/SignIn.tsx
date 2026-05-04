0f140d2575a8a0fd4e305611ed3795442d2cce93
Diogo Carvalho <ispg2022104527@ispgaya.pt>
2026-05-04 14:27:00 +0100

    useState signin

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
 export function SignIn(){
-    return(
-        <form className="w-full flex flex-col gap-4">
-            <Input required legend="E-mail" type="email" placeholder="seu@email.com" />
-            <Input required legend="Senha" type="password" placeholder="123456" />
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    
+    function onSubmit(e: React.FormEvent){
+        e.preventDefault()
 
-            <Button>Entrar</Button>
+        console.log(email, password)
+    }
+
+    return(
+        <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+            <Input required legend="E-mail" type="email" placeholder="seu@email.com" onChange={(e)=> setEmail(e.target.value)} />
+            <Input required legend="Senha" type="password" placeholder="123456" onChange={(e)=> setPassword(e.target.value)} />
+
+            <Button type="submit" isLoading={isLoading}>Entrar</Button>
         </form>
     )
 }
