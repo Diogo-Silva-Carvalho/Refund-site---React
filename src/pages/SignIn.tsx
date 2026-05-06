a834efac21da4f52a18597bc8667a5b4020346f3
Diogo Carvalho <ispg2022104527@ispgaya.pt>
2026-05-06 15:54:13 +0100

    Refactor SignIn component to utilize useActionState for form handling and remove local state management

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react"
+import { useActionState } from "react"
+
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
-export function SignIn(){
-    const [isLoading, setIsLoading] = useState(false)
-    
-    function onAction(formData: FormData){
-        console.log(formData.get("password"))
+export function SignIn(){    
+    const [state, formAction, isLoading] = useActionState(signIn, {email: "", password: ""})
+
+    async function signIn(prevState: any, formData: FormData){
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+        return { email, password }
     }
 
     return(
-        <form action={onAction} className="w-full flex flex-col gap-4">
-            <Input name="email" required legend="E-mail" type="email" placeholder="seu@email.com"/>
-            <Input name="password" required legend="Senha" type="password" placeholder="123456"/>
+        <form action={formAction} className="w-full flex flex-col gap-4">
+            <Input name="email" required legend="E-mail" type="email" placeholder="seu@email.com" defaultValue={String(state?.email)}/>
+            <Input name="password" required legend="Senha" type="password" placeholder="123456" defaultValue={String(state?.password)}/>
 
             <Button type="submit" isLoading={isLoading}>Entrar</Button>
 
