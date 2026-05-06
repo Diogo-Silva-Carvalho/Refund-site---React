464d7676dd16fbb6a2b5091ef0bfcbd47d6a12b6
Diogo Carvalho <ispg2022104527@ispgaya.pt>
2026-05-06 11:51:20 +0100

    Refactor Dashboard component layout and update heading text

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,9 @@
 export function Dashboard(){
     return(
-        <h1>Dasboard</h1>
+        <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768]">
+            <h1 className="text-gray-100 font-bold text-xl flex-1">
+                Solicitações
+            </h1>
+        </div>
     )
 }
