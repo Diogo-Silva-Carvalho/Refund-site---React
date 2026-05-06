1e43598a339ca923ec2f18479036a672946acd72
Diogo Carvalho <ispg2022104527@ispgaya.pt>
2026-05-06 10:57:01 +0100

    Add category selection to refund form and create categories utility

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -12,7 +12,7 @@ export function Select({legend, children, ...rest}: Props){
                 </legend>
             }
 
-            <select className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300" value="" {... rest}>
+            <select className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"  {... rest}>
                 <option value="" disabled hidden>Selecione</option>
                 {children}
             </select>
